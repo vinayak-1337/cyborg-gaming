2f62a4d45e62b62aa459d191c8ebf18f7c487460
vinayak <vinayak@gkmit.co>
2022-11-23 10:20:43 +0530

    Add mobile nav button

---
+++ script.js
@@ -0,0 +1,11 @@
+const mobileNavOpen = document.querySelector("#mobile-nav-open");
+const mobileNavClose = document.querySelector("#mobile-nav-close");
+const headerRight = document.querySelector("#header__right");
+
+mobileNavOpen.addEventListener("click", () => {
+  headerRight.style.width = "130px";
+});
+
+mobileNavClose.addEventListener("click", () => {
+  headerRight.style.width = "0";
+});
